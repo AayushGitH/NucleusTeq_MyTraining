e9f21ac42b63875311abdf78d0245bd0befc777e
Aayush <aayushtiwari160@gmail.com>
2023-03-10 16:20:20 +0530

    Generic program started

---
+++ week_5/Generics.java
@@ -0,0 +1,9 @@
+package Training.week_5;
+
+public class Generics 
+{
+	public static void main(String...args)
+	{
+		System.out.println("Program demonstrating the generics in Java programming language");
+	}
+}
