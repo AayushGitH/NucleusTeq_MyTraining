0cc7783c5b6e0d72f39d1dd06694f70af78347d2
Aayush <aayushtiwari160@gmail.com>
2023-03-16 22:09:46 +0530

    Final generic

--- week_5/Generics.java
+++ week_5/Generics.java
@@ -4,8 +4,28 @@ package Training.week_5;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// Generic class
+class Demo<T>
+{
+	T value;
+	T get()
+	{
+		return value;
+	}
+}
+
+// Concrete class
 public class Generics 
 {
+	
+	// Generic method
+	public static <E> void gdemo(E element)
+	{
+		System.out.println("The value of the generic element is = " + element);
+	}
+	
+	// Main method
 	public static void main(String...args)
 	{
 		System.out.println("Program demonstrating the generics in Java programming language");
@@ -36,5 +56,19 @@ public class Generics
 		System.out.println("The value of list3 second element is " + value);
 		System.out.println();
 		
+		// Making object of generic class
+		Demo<Integer> obj = new Demo();
+		obj.value = 9999;
+		System.out.println(obj.get());
+		
+		Demo<Double> dobj = new Demo();
+		dobj.value = 999.9000;
+		System.out.println(dobj.get());
+		
+		// Calling generic method
+		gdemo("Aayush");
+		gdemo(123456);
+		gdemo(1222222.2222);
+		gdemo('A');
 	}
 }
