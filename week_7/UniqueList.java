4160b85ef917b1bcbdb4a37c6dabdf999feaaff2
Aayush <aayushtiwari160@gmail.com>
2023-03-24 19:40:07 +0530

    Added distinct list

--- week_7/UniqueList.java
+++ week_7/UniqueList.java
@@ -1,9 +1,25 @@
 package Training.week_7;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class UniqueList 
 {
 	public static void main(String...args)
 	{
-		System.out.println("Hello this is the program of storing unique values in an Arraylist");
+		System.out.println("Enter 10 values to store in an Arraylist unique");
+		Scanner sc = new Scanner(System.in);
+		List<String> list = new ArrayList<>();
+		for(int i=0; i<10; i++)
+		{
+			String value = sc.nextLine();
+			list.add(value);
+		}
+		
+		// For storing distinct values in a list
+		list = list.stream().distinct().collect(Collectors.toList());
+		System.out.println(list);
 	}
 }
