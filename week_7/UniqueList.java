9b5b599f51596becc196a0751a372a28897d34cc
Aayush <aayushtiwari160@gmail.com>
2023-03-24 19:33:29 +0530

    First task of week 7

---
+++ week_7/UniqueList.java
@@ -0,0 +1,9 @@
+package Training.week_7;
+
+public class UniqueList 
+{
+	public static void main(String...args)
+	{
+		System.out.println("Hello this is the program of storing unique values in an Arraylist");
+	}
+}
